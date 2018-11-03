0d0ceb2abe13163602fff468f18e02002b474c7c
hkim <muncok@dal.snu.ac.kr>
2018-11-04 00:01:02 +0900

    minor fix

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ python run_trial.py -n_enr $n_enr -out_dir "$out_dir/base" -sv_mode base -n_proc
 
 for i in 1 2 3 4 5
 do
-    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$1" -sv_mode inc -n_process 80
-    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$1" -sv_mode inc -n_process 80 -incl_init
-    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$1" -sv_mode inc -n_process 80 -update
-    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$1" -sv_mode inc -n_process 80 -incl_init -update
+    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$i" -sv_mode inc -n_process 80
+    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$i" -sv_mode inc -n_process 80 -incl_init
+    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$i" -sv_mode inc -n_process 80 -update
+    python run_trial.py -n_enr $n_enr -out_dir "$out_dir/inc/$i" -sv_mode inc -n_process 80 -incl_init -update
 done
 
